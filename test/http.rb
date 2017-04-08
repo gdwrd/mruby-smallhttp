f1e5b9264a634a0a210a5aff38c6a3193d93169c
Nazarii Sheremet <nazarii.sheremet@gmail.com>
2017-04-08 14:03:05 +0300

    added more tests

--- test/http.rb
+++ test/http.rb
@@ -23,7 +23,21 @@ assert("make a request") do
 end
 
 assert_equal("response should be parsed") do
-  response = HTTP.new("http://mruby.org/about/").get
+  response = HTTP.new("http://www.mocky.io/v2/58e8c0ff120000731859b69d").get
 
   assert_equal response.status, 200
+end
+
+assert_equal("request headers should have key") do
+  response = HTTP.new("http://www.mocky.io/v2/58e8c1501200006e1859b69f").post
+
+  assert_equal response.headers.keys.include?("Key"), true
+  assert_equal response.headers.values.include?("Value"), true
+end
+
+assert_equal("request body should parsed as JSON") do
+  response = HTTP.new("http://www.mocky.io/v2/58e8c2071200008a1859b6a0").put
+
+  assert_equal JSON::parse(response.body)["data"], "data"
+  assert_equal JSON::parse(response.body)["status"], 200
 end
